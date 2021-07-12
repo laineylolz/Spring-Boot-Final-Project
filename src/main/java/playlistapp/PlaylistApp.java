e27f177ef72671276e743ef33813a1f32188890b
laineylolz <81186855+laineylolz@users.noreply.github.com>
2021-07-12 16:39:39 -0500

    Project created, .pomx created, application created

---
+++ src/main/java/playlistapp/PlaylistApp.java
@@ -0,0 +1,14 @@
+package playlistapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistApp {
+
+		public static void main(String[] args) {
+			SpringApplication.run(PlaylistApp.class, args);
+
+	}
+
+}
